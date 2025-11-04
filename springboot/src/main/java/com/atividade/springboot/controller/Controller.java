acef4efd5cb19ed7d277857dd84d076c625445b7
Larissa Pinheiro <116392603+LariGranger@users.noreply.github.com>
2025-11-04 22:47:29 +0000

    springboot

---
+++ springboot/src/main/java/com/atividade/springboot/controller/Controller.java
@@ -0,0 +1,31 @@
+package com.atividade.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class Controller {
+    @GetMapping("/api")
+    public String home(){
+        return "Olá, SpringBoot";
+    }
+    @GetMapping("/api/soma/{numUm}/{numDois}")
+    public int soma(@PathVariable int numUm, @PathVariable int numDois){
+        return numUm + numDois;
+    }
+    @GetMapping("/api/subtracao/{numUm}/{numDois}")
+    public int subtracao(@PathVariable int numUm, @PathVariable int numDois){
+        return numUm - numDois;
+    }
+    @GetMapping("/api/verificaParImpar/{numUm}")
+    public String verificaParImpar(@PathVariable int numUm){
+        if(numUm % 2 == 0){
+            return "O número " + numUm + "é par";
+        }else{
+            return "O número " + numUm + "é impar";
+        }
+        
+    }
+}
